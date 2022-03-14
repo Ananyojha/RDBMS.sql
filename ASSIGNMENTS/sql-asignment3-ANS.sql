634cc3d01da42076ebca4acd0d7d05bd592b99f3
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-03-14 13:28:27 +0530

    Update sql-asignment3-ANS.sql

--- ASSIGNMENTS/sql-asignment3-ANS.sql
+++ ASSIGNMENTS/sql-asignment3-ANS.sql
@@ -1,35 +1,36 @@
-Select GetDate() AS 'CurrentDATETime
+--SQL Assignment 3 : System Defined Functions
 
---
+
+--1>Display the Return the current date and time both 
+Select GetDate() AS 'Current DATE and Time'
+
+--2> Find out the index of 't' from a string Customer
 SELECT 
-    CHARINDEX('t', 'customer') position;
+    CHARINDEX('t', 'customer') 'position of t';
 
--- 
+--3> Find out the lenth of 'Synergetics Pvt Ltd'
 Select LEN('Synergetics Pvt Ltd')
 
--- 
+--4> Convert the text to lower case  'Synergetics Pvt Ltd!!' 
+Select LOWER('Synergetics Pvt Ltd').
 
-Select LOWER('Synergetics Pvt Ltd')
-
---
-
+--5> display the reverse of 'Synergetics Pvt Ltd'.
 SELECT REVERSE('Synergetics Pvt Ltd')
 
--- 
+--6>Display only  'Synergetics' from text ' Synergetics Pvt Ltd'.
 
-SELECT ROUND(12.3456, 2);
 
--- 
+--7>Find 6 raised to the third power value 
 Select power(6,3) 
 
--- 
-SELECT DATEADD(year, 1, '2017/08/25') AS DateAdd
+--8> Round the number 456.789  to 2 decimal places:
+SELECT ROUND(456.789, 2)
 
--- 
-SELECT DATEDIFF(year, '2017/08/25', '2011/08/25') AS DateDiff;
+--9>Add one year to a date - 2020/03/13 , then return the date:
+SELECT DATEADD(year, 1, '2017/08/25') 
 
--- 
-CAST ( expression AS data_type 
+--10>Display the difference between  two date i.e 2020/03/13 and  2015/08/25 , in years:
+SELECT DATEDIFF(year, '2020/03/13', '2015/08/25')
 
-
-
+--11> convert a string '2020-03-10' to a datetime value -  Using the CAST() function 
+cast('2020/03/10' as datetime)
