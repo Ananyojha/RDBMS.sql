27a227c408b4a065c0127e84007e0c58958f02e0
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-03-14 13:03:55 +0530

    Update sql-asignment3-ANS.sql

--- ASSIGNMENTS/sql-asignment3-ANS.sql
+++ ASSIGNMENTS/sql-asignment3-ANS.sql
@@ -17,7 +17,19 @@ SELECT REVERSE('Synergetics Pvt Ltd')
 
 -- 
 
+SELECT ROUND(12.3456, 2);
 
+-- 
+Select power(6,3) 
+
+-- 
+SELECT DATEADD(year, 1, '2017/08/25') AS DateAdd
+
+-- 
+SELECT DATEDIFF(year, '2017/08/25', '2011/08/25') AS DateDiff;
+
+-- 
+CAST ( expression AS data_type 
 
 
 
