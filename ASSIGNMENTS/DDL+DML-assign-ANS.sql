e79788f7ef6f389a4eba10eef92bd2d624d16974
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-03-15 12:14:08 +0530

    Create DDL+DML-assign-ANS.sql

---
+++ ASSIGNMENTS/DDL+DML-assign-ANS.sql
@@ -0,0 +1,26 @@
+/*
+1. Create the table Customer with the following Columns:
+CustomerId		int ,
+CustomerName  	Varchar(30),
+Address1		Varchar (30)
+Address2		varchar (30)
+
+
+2. Modify the customer table Customername column of datatype with varchar2(30) and Name 
+Should not accept Nulls
+
+
+3. Add the following columns to the customer table 
+Sex		varchar(1)
+Age 		int
+PhoneNo 	int
+Income 		int 
+
+4. Insert rows with the following data into the customer table 
+1000,Allen,#115Chicago,#115Chicago,M,25,7878776,1000
+1001,George,#116France,#114France,M,25,434524,2000
+1002,Becker,#114 New York,#114 New York,M,45,431525,15000
+
+
+
+5. change the income of customer Id 1002 from 15000 to 25000 */
