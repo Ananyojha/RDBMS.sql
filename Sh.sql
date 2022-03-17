d845e80e81d4cbb76d433f80a1b9b2f1003e85ad
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-03-17 16:46:32 +0530

    Update Sh.sql

--- Sh.sql
+++ Sh.sql
@@ -1,3 +1,12 @@
 SELECT * 
 FROM alter.table
 WHERE job = 'Analyst' 
+/*
+Rename default.conf to default.conf.bak
+
+Create a file called server1.conf with this configuration in it:
+
+server { root /home/ubuntu/public_html; location /application1 { } location /images { root /home/ubuntu/data; } }
+
+
+*/
