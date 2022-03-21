13210c06d5c2cd53cd1c03f788c61cc0bec50c71
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-03-21 20:13:43 +0530

    Update and rename Sh.sql to Mock.sql

--- Mock.sql
+++ Mock.sql
@@ -1,7 +1,24 @@
-SELECT * 
+/* 
+*******************  JOINS  *******************
+*/
+
+-- 1. 
+
+
+
+
+
+
+
+
+
+
+
+
+/* SELECT * 
 FROM alter.table
 WHERE job = 'Analyst' 
-/*
+
 Rename default.conf to default.conf.bak
 
 Create a file called server1.conf with this configuration in it:
