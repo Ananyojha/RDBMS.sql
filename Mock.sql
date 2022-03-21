58b0e79af7e071a74b6320710777452d7894e3af
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-03-21 21:37:14 +0530

    Update Mock.sql

--- Mock.sql
+++ Mock.sql
@@ -2,7 +2,38 @@
 *******************  JOINS  *******************
 */
 
--- 1. 
+-- CREATE ENVIRONMENT
+
+CREATE SCHEMA hr;
+
+CREATE TABLE hr.candidates(
+    id INT PRIMARY KEY IDENTITY,
+    fullname VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE hr.employees(
+    id INT PRIMARY KEY IDENTITY,
+    fullname VARCHAR(100) NOT NULL
+);
+
+
+INSERT INTO 
+    hr.candidates(fullname)
+VALUES
+    ('John Doe'),
+    ('Lily Bush'),
+    ('Peter Drucker'),
+    ('Jane Doe');
+
+
+INSERT INTO 
+    hr.employees(fullname)
+VALUES
+    ('John Doe'),
+    ('Jane Doe'),
+    ('Michael Scott'),
+    ('Jack Sparrow');
+
 
 
 
