bf1062dbc0337918f887e9787759a8b072c30487
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-03-23 13:45:21 +0530

    Update Mock.sql

--- Mock.sql
+++ Mock.sql
@@ -1,124 +1,57 @@
-/* 
-*******************  JOINS  *******************
+select * from emp;
+select * from Dept;
+select * from salgrade;
+
+
+
+-- *********** JOINS ************
+
+
+
+
+
+/*
+Find all employees who have been 
+assigned the departments. 
 */
 
--- CREATE ENVIRONMENT
 
-CREATE SCHEMA hr;
-
-CREATE TABLE hr.candidates(
-    id INT PRIMARY KEY IDENTITY,
-    fullname VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE hr.employees(
-    id INT PRIMARY KEY IDENTITY,
-    fullname VARCHAR(100) NOT NULL
-);
+-- 
+/*
+Find out all departments and thier respective employees
+*/
 
 
-INSERT INTO 
-    hr.candidates(fullname)
-VALUES
-    ('John Doe'),
-    ('Lily Bush'),
-    ('Peter Drucker'),
-    ('Jane Doe');
+
+-- 
+/* Find out all employees and there respective departments*/
 
 
-INSERT INTO 
-    hr.employees(fullname)
-VALUES
-    ('John Doe'),
-    ('Jane Doe'),
-    ('Michael Scott'),
-    ('Jack Sparrow');
 
-/* CHECK TABLES :
+-- 
 
-SELECT * FROM hr.candidate 
-SELECT * FROM hr.employees
+/* Find all details about every employee and every department */
+
+
+
+
+-- Cross join 
+
+
+
+-- Display the names and job titles of all employees with the same job as Jones.
+
+
+
+
+-- List the employee details who earn highest salary for their job.
+
+
+/*
+Correlated subqueries are used for row-by-row processing. 
+Each subquery is executed once for every row of the outer query.
 
 */
 
--- Question 
 
--- ************ USING INNER JOIN ************
-
-SELECT  
-    c.id candidate_id,
-    c.fullname candidate_name,
-    e.id employee_id,
-    e.fullname employee_name
-FROM 
-    hr.candidates c
-    INNER JOIN hr.employees e 
-        ON e.fullname = c.fullname;
-
--- Question
-
--- ********* USING LEFT JOIN ***********
-
-SELECT  
-	c.id candidate_id,
-	c.fullname candidate_name,
-	e.id employee_id,
-	e.fullname employee_name
-FROM 
-	hr.candidates c
-	LEFT JOIN hr.employees e 
-		ON e.fullname = c.fullname;
-
-
--- Question
-
--- ********* USING RIGHT JOIN ***********
-
-SELECT  
-    c.id candidate_id,
-    c.fullname candidate_name,
-    e.id employee_id,
-    e.fullname employee_name
-FROM 
-    hr.candidates c
-    RIGHT JOIN hr.employees e 
-        ON e.fullname = c.fullname;
-
-
--- Question
-
--- ********** USING  FULL JOIN  **********
-
-SELECT  
-    c.id candidate_id,
-    c.fullname candidate_name,
-    e.id employee_id,
-    e.fullname employee_name
-FROM 
-    hr.candidates c
-    FULL JOIN hr.employees e 
-        ON e.fullname = c.fullname;
-
-
--- ****** USING  CROSS JOIN *********
-
-TABLE SHOWS ERROR WITHOUT SPECIFING IN FROM
-
-
--- ******** SUBQUERY ********
-
-
-
-
-/* SELECT * 
-FROM alter.table
-WHERE job = 'Analyst' 
-
-Rename default.conf to default.conf.bak
-
-Create a file called server1.conf with this configuration in it:
-
-server { root /home/ubuntu/public_html; location /application1 { } location /images { root /home/ubuntu/data; } }
-
-
-*/
+   
